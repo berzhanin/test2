b28b3b8580b6e3957ba2dd7b90ff883a4864a320
Dmitry Berzhanin <di.berzhanin@gmail.com>
2018-10-25 00:49:34 +0300

    add files to task1

---
+++ task1/HelloDmitry.java
@@ -0,0 +1,9 @@
+package tests.task1;
+
+public class HelloDmitry{
+    public static void main(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            System.out.println("Hello, " + args[i]);
+        }
+    }
+}
---
+++ task1/HelloWorld.java
@@ -0,0 +1,7 @@
+package tests.task1;//package task1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
